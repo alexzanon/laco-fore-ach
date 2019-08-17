c4474e8d43be590057264953bf6abc7d20f98aa0
Alex Zanon <alexzanonvl@hotmail.com>
2019-08-17 11:36:47 -0300

    Demonstração de laços

---
+++ src/sec5_laco_for_each/Program.java
@@ -0,0 +1,19 @@
+package sec5_laco_for_each;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String[] vect = new String[] {"Maria", "Bob", "Alex"};
+		
+		//for(int i=0; i<vect.length; i++) {
+		//	System.out.println(vect[i]);
+		//}
+
+		//mesmo efeito, transforma cada objeto do vetor em obj
+		for (String obj : vect) {
+			System.out.println(obj);
+		}
+	}
+
+}
